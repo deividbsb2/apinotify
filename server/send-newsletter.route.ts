bdffc7581dd7e10fa4c54d615cf2d01723f64775
Fernandes <rffernandes@indra.es>
2018-06-07 14:40:39 -0300

    Mensagem vinda do frontend

--- server/send-newsletter.route.ts
+++ server/send-newsletter.route.ts
@@ -4,28 +4,27 @@ const webpush = require('web-push');
 
 
 export function sendNewsletter(req, res) {
-
     console.log('Total subscriptions', USER_SUBSCRIPTIONS.length);
 
     // sample notification payload
     const notificationPayload = {
         "notification": {
-            "title": "Angular News",
-            "body": "Newsletter Available!",
-            "icon": "assets/main-page-logo-small-hat.png",
+            "title": "ISF - Notificação",
+            "body": req.body.mensagem,
+            "icon": "assets/img/logo-sem-nome.png",
             "vibrate": [100, 50, 100],
             "data": {
                 "dateOfArrival": Date.now(),
                 "primaryKey": 1
             },
             "actions": [{
-                "action": "explore",
-                "title": "Ir para o site"
+                "action": "onClick",
+                "title": "Ir para o site",
+                'url': 'https://aluno-2.firebaseapp.com'
             }]
         }
     };
-
-
+    console.log(webpush);
     Promise.all(USER_SUBSCRIPTIONS.map(sub => webpush.sendNotification(
         sub, JSON.stringify(notificationPayload) )))
         .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
@@ -33,11 +32,5 @@ export function sendNewsletter(req, res) {
             console.error("Error sending notification, reason: ", err);
             res.sendStatus(500);
         });
-
-
-
-
-
-
 }
 
